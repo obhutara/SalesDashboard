5c3c31285376860ad29b0f7f195a740037d7e9b6
unknown <obhutara@gmail.com>
2024-03-04 14:56:56 +0530

    updated 2/3/24

--- src/app.py
+++ src/app.py
@@ -53,9 +53,9 @@ app.layout = html.Div([
     dcc.DatePickerRange(
         id='date-picker-range',
         min_date_allowed=dt(2022, 1, 1),
-        max_date_allowed=dt(2024, 1, 31),
-        start_date=dt(2024, 1, 1),
-        end_date=dt(2024, 1, 31)
+        max_date_allowed=dt(2024, 2, 29),
+        start_date=dt(2024, 2, 1),
+        end_date=dt(2024, 2, 29)
     ),
     html.Div(children=[
         html.Label('Checkboxes'),
