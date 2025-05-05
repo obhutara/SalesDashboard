20c5817d6405735a01e04a8af03a3a4777736f55
unknown <obhutara@gmail.com>
2025-05-05 17:45:20 +0530

    updated 4/30

--- src/app.py
+++ src/app.py
@@ -54,8 +54,8 @@ app.layout = html.Div([
         id='date-picker-range',
         min_date_allowed=dt(2022, 1, 1),
         max_date_allowed=dt(2025, 3, 31),
-        start_date=dt(2025, 3, 1),
-        end_date=dt(2025, 3, 31)
+        start_date=dt(2025, 4, 1),
+        end_date=dt(2025, 4, 30)
     ),
     html.Div(children=[
         html.Label('Checkboxes'),
