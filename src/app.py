6adad4b5f91f9c8084889dff7e36cf87b21e422c
Omkar Bhutra <obhutara@gmail.com>
2025-09-01 17:28:38 +0530

    updated 31/8

--- src/app.py
+++ src/app.py
@@ -53,7 +53,7 @@ app.layout = html.Div([
     dcc.DatePickerRange(
         id='date-picker-range',
         min_date_allowed=dt(2022, 1, 1),
-        max_date_allowed=dt(2025, 7, 31),
+        max_date_allowed=dt(2025, 8, 31),
         start_date=dt(2025, 8, 1),
         end_date=dt(2025, 8, 31)
     ),
